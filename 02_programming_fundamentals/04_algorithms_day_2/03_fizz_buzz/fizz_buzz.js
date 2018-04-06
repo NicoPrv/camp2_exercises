f7e74d6b9f850979c1501c4ac28fd10a47cb0560
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-06 17:43:38 +0200

    Exercise FizzBuzz OK

--- 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
+++ 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
@@ -1,16 +1,35 @@
 /* Implement a fizzBuzz function as such: `fizzBuzz(list)`, where `list` is an array
-   of positive integers, for example : `[1, 2, 3, 4, 5, 6]`.
-   This function will return a new array where some values will have been modified:
+of positive integers, for example : `[1, 2, 3, 4, 5, 6]`.
+This function will return a new array where some values will have been modified:
 
-   - if the number is divisible by 3: `Fizz`;
-   - if the number is divisible by 5: `Buzz`;
-   - if the number is divisible by 3 and 5 : `FizzBuzz`
-   - otherwise, the value is preserved.
+- if the number is divisible by 3: `Fizz`;
+- if the number is divisible by 5: `Buzz`;
+- if the number is divisible by 3 and 5 : `FizzBuzz`
+- otherwise, the value is preserved.
 */
 
+
+
 function fizzBuzz(list) {
+  const newArray=list.map(functionFb);
+  return newArray;
+  //console.log(newArray);
 
 }
 
+
+function functionFb(number){
+  if (number %3 === 0 && number %5 ===0){
+    return "FizzBuzz";
+  }else if(number %3 === 0){
+    return "Fizz";
+  }else if(number %5 ===0){
+    return "Buzz";
+  }else{
+    return number;
+  }
+}
+
+console.log(fizzBuzz([1,2,3,4,5,6,10,15,20,33]));
 
 module.exports = fizzBuzz;
