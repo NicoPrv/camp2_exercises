6f096c8080cdcaff9c44ea5e4758770c3a52cfd0
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-06 16:58:43 +0200

    Exercise 01 range, dur dur :)

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,9 +4,31 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
+function range(number1, number2) {
+
+  const myArray=[];
+  //  myArray.push (number1);
+
+  if(number1 > number2){
+    for(let i=number1 ; i>=number2 ; i--){
+      myArray.push(i);
+    }
+
+  }else if(number2 > number1){
+    for(let i=number1 ; i<=number2 ; i++){
+      myArray.push(i);
+    }
+
+    
+
+  }
+
+  return myArray;
 
 }
+
+console.log(range(3,10));
+
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
