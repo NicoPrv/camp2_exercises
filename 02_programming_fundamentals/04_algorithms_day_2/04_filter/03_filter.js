76fc1a1c492da593d02196244674a96205d73f46
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-08 18:15:05 +0200

    Exercise 03 filter, seems to work but test is KO..

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/03_filter.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/03_filter.js
@@ -2,7 +2,29 @@
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
 function filter(array, fn) {
   // Your code here
+
+  // I create an empty array result that will contain my answer
+  let result=[];
+
+  // I check that functions inputs were Ok
+  if(array.length !==0 && fn===pickEvenNumbers){
+    // If OK, I filter the array
+    result=array.filter(fn);
+    //If not, I manage the case
+  }else{
+    return("tableau vide",array);
+  }
+  //I return the filtered array
+  return result;
 }
+
+function pickEvenNumbers(number){
+  // I check that the number is even or not
+  return(number %2 === 0);
+}
+
+// Personal test seems to work fine
+console.log(filter([0,1,456,786,51,2,3,4],pickEvenNumbers));
 
 // do not remove this line, it is for tests
 module.exports = filter;
