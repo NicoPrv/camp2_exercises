75bbf70fdeb90e3bd41b3c278854b2259eb4a7fe
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-08 17:55:55 +0200

    Exercise filter with function. Behaviour OK but test NOK

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
@@ -1,8 +1,31 @@
 // filter takes an array of integer and a function of filtering
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
-function filter(array, fn) {
-  // Your code here
-}
 
+// Your code here
+let result ;
+function filter (myArray, func) {
+  result=[];
+  if(myArray.length===0){
+    console.log("Tableau vide");
+  }else{
+    myArray.forEach (func);
+  }
+  return result;
+}
+function pickEvenNumbers (number) {
+  if (number % 2 === 0){
+    result.push(number) ;
+  }
+  //return result;
+}
+function pickOddNumbers (number) {
+  if (number % 2 !== 0){
+    result.push(number) ;
+  }
+  //return result;
+}
+console.log(filter([0,2,4],pickEvenNumbers));
+
+//console.log(filter([0,2,4],pickEvenNumbers));
 // do not remove this line, it is for tests
 module.exports = filter;
