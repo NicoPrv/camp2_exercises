71cf7a1d9106ace7bda813fac77df1b3e7476c88
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-08 15:30:16 +0200

    01 filter with string exercise

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
@@ -1,8 +1,40 @@
 // filter takes an array of integer and a string with either odd or even
 // such as filter([1, 2, 3, 4, 5], 'even') returns [2, 4]
-function filter(array, str) {
-  // Your code here
+
+// Your code here
+
+
+//console.log ( filter ([1,3,4,5,19,22,67],"even") );
+let result = [] ;
+function filter (myArray, oddOrEven) {
+
+  if (myArray !== []){
+
+    if (oddOrEven === "even") {
+      myArray.forEach (filterEven);
+    } else if (oddOrEven === "odd"){
+      myArray.forEach (filterOdd);
+    }
+
+    return result;
+
+  }
+
+  function filterEven (number) {
+    if (number % 2 === 0){
+      result.push(number) ;
+    }
+  }
+
+  function filterOdd (number) {
+    if (number % 2 !== 0){
+      result.push(number) ;
+    }
+  }
+
 }
+
+console.log(filter([1,2,3,4,5], "odd"));
 
 // do not remove this line, it is for tests
 module.exports = filter;
