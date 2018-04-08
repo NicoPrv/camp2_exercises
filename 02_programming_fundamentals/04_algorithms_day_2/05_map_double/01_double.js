359fa574ba7067dca4dc7d037482e5bb2417b76b
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-08 18:55:49 +0200

    Exercise 01_double.js. It works but test is KO, don't understand why

--- 02_programming_fundamentals/04_algorithms_day_2/05_map_double/01_double.js
+++ 02_programming_fundamentals/04_algorithms_day_2/05_map_double/01_double.js
@@ -3,6 +3,24 @@
 
 // Your code here...
 
+function double(array){
+  // I create an empty array result that will contain the output
+  result=[];
+  // I iterate through the input array
+  for (let i=0;i<array.length;i++){
+    // I push in my result array the doubles values
+    result.push(array[i]*2);
+  }
+  //test is OK
+  console.log(result);
+  // I return the result
+  return result;
+}
+
+// Test
+double([1,2,3]);
+
+
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = double;
