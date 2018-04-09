c15a3c68050e229632d28573988089a65588362b
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-09 21:21:24 +0200

    Exo FizzBuzz OK

--- 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
+++ 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
@@ -9,26 +9,29 @@ This function will return a new array where some values will have been modified:
 */
 
 
+//On met les trucs dont on a besoin avant d'en avoir besoin...
+//Traiter les cas les plus spécifiques en premier
+function functionFb(number){
+  let result;
+  if (number %3 === 0 && number %5 === 0){
+    return "FizzBuzz";
+  }else if(number %3 === 0){
+    return "Fizz";
+  }else if(number %5 === 0){
+    return "Buzz";
+  }else{
+    result = number ;
+    return number;
+  }
+}
 
 function fizzBuzz(list) {
   const newArray=list.map(functionFb);
   return newArray;
   //console.log(newArray);
-
 }
 
 
-function functionFb(number){
-  if (number %3 === 0 && number %5 ===0){
-    return "FizzBuzz";
-  }else if(number %3 === 0){
-    return "Fizz";
-  }else if(number %5 ===0){
-    return "Buzz";
-  }else{
-    return number;
-  }
-}
 
 console.log(fizzBuzz([1,2,3,4,5,6,10,15,20,33]));
 
