e8c8f113d8aff0008ee94c8421c6e9865adf4184
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-09 13:30:55 +0200

    03 square digits OK :)

--- 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
+++ 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
@@ -7,10 +7,28 @@
 // Note: This is not just the square of a number but the square of each digits
 // Note: The function accepts an integer and returns an integer
 
+
+
+
 function squareDigits(number) {
+  let numbersToSquarify = [];
+  let str = number.toString();
+  for (let i=0 ; i<str.length ; i++){
+    numbersToSquarify.push(parseInt(str.charAt(i),10));
+  }
+  //console.log(result);
+  let resultSquared=(numbersToSquarify.map((number)=>number*number));
+  return(parseInt(resultSquared.join(""),10));
+
   // Your code here
 }
 
+
+
+
+
+
+squareDigits(12);
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = squareDigits;
