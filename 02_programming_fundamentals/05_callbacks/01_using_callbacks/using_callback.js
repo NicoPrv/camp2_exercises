a9902648299dc33fec7d8adc4f544eda947ab0c4
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-09 12:11:42 +0200

    01 using callbacks OK

--- 02_programming_fundamentals/05_callbacks/01_using_callbacks/using_callback.js
+++ 02_programming_fundamentals/05_callbacks/01_using_callbacks/using_callback.js
@@ -1,8 +1,14 @@
 const thisIsThePlayer = (callback) => {
   const player = { name: "Spartacus", life: 100, strength: 100, agility: 100 };
   callback(player);
-}
+};
 
 // Write a function sayHello that greets: "Hello, Spartacus". (Where "Spartacus" is the player name)
+
+function sayHello (object) {
+  //.log("ça passe");
+  //console.log("Hello, " + object.name);
+  return ("Hello, " + object.name) ;
+}
 
 thisIsThePlayer(sayHello);
