aa674217d72a1322fe73e27a15436eda01fb2c9e
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-11 16:40:07 +0200

    delete file

--- 02_programming_fundamentals/07_file_management/01_basic_fs/02_delete_file.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/02_delete_file.js
@@ -2,5 +2,34 @@
 // if it's a file (do not remove a folder).
 //
 // The function returns a boolean indicating if it successfully removed the file.
+const path = require("path");
+const fs = require("fs");
 
-module.exports = deleteFile
+
+
+
+
+function deleteFile(filePath){
+  let isFileRemoved;
+
+
+  try {
+    fs.unlinkSync(filePath);
+    return true;
+  } catch (err) {
+    // handle the error
+    return false;
+  }
+
+
+}
+
+
+deleteFile("testToDelete.txt");
+
+
+
+
+
+
+module.exports = deleteFile;
