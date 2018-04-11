b6ac799675d59de486f2be74c8095aa0471cb763
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-11 16:40:46 +0200

    cut file

--- 02_programming_fundamentals/07_file_management/01_basic_fs/04_cut_file.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/04_cut_file.js
@@ -3,4 +3,51 @@
 // Implement the function cutPaste(sourceFilename, targetFilename)
 
 
+
+const path = require("path");
+const fs = require("fs");
+const { COPYFILE_EXCL } = fs.constants;
+
+function cutPaste(sourceFilename, targetFilename){
+
+  fs.stat(sourceFilename, (err,stats)=>{
+    if(err){
+      console.log("error");
+    }else{
+      console.log(stats.isDirectory(sourceFilename));
+      if(stats.isDirectory(sourceFilename)){
+        console.log("We don't want to cut a repertory");
+        return;
+      }
+    }
+  });
+
+
+  fs.copyFile(sourceFilename, targetFilename, COPYFILE_EXCL,(err) => {
+    if (err){
+      if(err.code==="EEXIST"){
+        console.log (`File ${targetFilename} already exists. We don't want to override it`);
+        return;
+      }else{
+        console.log (err);
+        return;
+      }
+    }
+
+    else{
+      //console.log(`${sourceFilename} was copied to ${targetFilename}`);
+
+      fs.unlink("./"+sourceFilename, (err) => {
+        if (err) throw err;
+        console.log(`${sourceFilename} was cut to ${targetFilename}`);
+      });
+
+      return;
+    }
+  });
+
+} // END OF copyPaste function
+
+cutPaste ("testText2.txt", "testText6.txt");
+
 module.exports = cutPaste;
