7745cc29992522789043d737d24da84dff59aaae
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-11 16:40:25 +0200

    copy file

--- 02_programming_fundamentals/07_file_management/01_basic_fs/03_copy_file.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/03_copy_file.js
@@ -4,4 +4,43 @@
 //
 // The function returns a boolean indicating if it successfully removed the file.
 
+const path = require("path");
+const fs = require("fs");
+const { COPYFILE_EXCL } = fs.constants;
+
+function copyPaste(sourceFilename, targetFilename){
+
+  fs.stat(sourceFilename, (err,stats)=>{
+    if(err){
+      console.log("error");
+    }else{
+      console.log(stats.isDirectory(sourceFilename));
+      if(stats.isDirectory(sourceFilename)){
+        console.log("We don't want to copy a repertory");
+        return;
+      }
+    }
+  });
+
+
+  fs.copyFile(sourceFilename, targetFilename, COPYFILE_EXCL,(err) => {
+    if (err){
+      if(err.code==="EEXIST"){
+        console.log (`File ${targetFilename} already exists. We don't want to override it`);
+        return;
+      }else{
+        console.log (err);
+        return;
+      }
+    }
+
+    else{
+      console.log(`${sourceFilename} was copied to ${targetFilename}`);
+      return;
+    }
+  });
+
+} // END OF copyPaste function
+
+copyPaste ("testRepertory", "testText2.txt");
 module.exports = copyPaste;
