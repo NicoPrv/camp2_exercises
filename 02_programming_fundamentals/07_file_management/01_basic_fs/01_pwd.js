9a2f508741351def32c955ed2dcece0ac08df0d5
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-11 16:39:35 +0200

    01 pwd OK

--- 02_programming_fundamentals/07_file_management/01_basic_fs/01_pwd.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/01_pwd.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 
 // Add a function `pwd` which takes no arguments and return the current folder name we are in
 //
@@ -6,4 +7,17 @@ const path = require("path");
 //
 // pwd() # => "/Users/john/Workspace/my_folder"
 
-module.exports = pwd
+
+
+
+function pwd(){
+  console.log(path.resolve("."));
+  return path.resolve(".");
+}
+
+pwd();
+
+
+
+
+module.exports = pwd;
