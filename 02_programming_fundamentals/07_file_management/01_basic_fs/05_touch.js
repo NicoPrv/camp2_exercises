a0f5637bbbb1cbfbccd93b8b6c5d9284351b589a
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-11 16:41:09 +0200

    touch

--- 02_programming_fundamentals/07_file_management/01_basic_fs/05_touch.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/05_touch.js
@@ -1,4 +1,36 @@
 // Using the file creation commands, create a touch function that mimics the behavior of the Unix command.
+const path = require("path");
+const fs = require("fs");
+const { COPYFILE_EXCL } = fs.constants;
+
+
+function touch(fileName){
+  //check there is not already a file with this name
+
+
+  
+  fs.open(fileName, 'wx', (err, file) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        console.error(`file ${fileName} already exists!`);
+        return;
+      }
+
+      throw err;
+    }else{
+
+
+      writeMyData(file);
+    }
+  });
+
+
+} // END OF copyPaste function
+
+touch ("testPouet.txt");
+
+
+
 
 
 
