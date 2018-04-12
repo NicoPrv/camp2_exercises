e760cff99c2a7a18bb64ae74c238897fa099cd12
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-12 14:10:57 +0200

    03 coffee machine OK

--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -1,14 +1,58 @@
 // See Sparta courses for the exercise summary
 
+
+const machine = {
+
+  litersOfCoffee:0,
+
+  fillWithLitersOfCoffee:function(liters){
+    this.litersOfCoffee += liters ;
+    console.log(this);
+  },
+
+  expresso:function(){
+    if(this.litersOfCoffee*100 >= 8){
+      this.litersOfCoffee -= (8/100) ;
+      console.log(this);
+      return true ;
+      //console.log("I serve you an expresso!");
+
+    }
+    return false ;
+    //console.log(this);
+
+  },
+
+  longCoffee:function(){
+    //console.log(this.litersOfCoffee*100);
+    if(this.litersOfCoffee*100 >= 15){
+      this.litersOfCoffee -= (15/100) ;
+      console.log(this);
+      return true ;
+    }
+    return false ;
+  }
+
+
+};
+
+//console.log(machine.fillWithLitersOfCoffee(10));
+
 // Coffee Machine usage. Insert your code above this comment
 
+// machine.litersOfCoffee = 0.33;
+// console.log(machine);
+// console.log(machine.longCoffee());
+
+
+
 machine.fillWithLitersOfCoffee(10);
-console.log(machine.expresso()) // => true
-console.log(machine.litersOfCoffee) // => 9.92
-console.log(machine.longCoffee()) // => true
-console.log(machine.litersOfCoffee) // => 9.77
+console.log(machine.expresso()); // => true
+console.log(machine.litersOfCoffee); // => 9.92
+console.log(machine.longCoffee()); // => true
+console.log(machine.litersOfCoffee); // => 9.77
 // some more people ordering coffee here
-console.log(machine.litersOfCoffee) // => 0.02
-console.log(machine.expresso()) // => false
+console.log(machine.litersOfCoffee); // => 0.02
+console.log(machine.expresso()); // => false
 
 module.exports = machine;
