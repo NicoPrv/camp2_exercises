5221dc3391ddca37c822ed2626e39862d5b6ad55
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-12 17:01:13 +0200

    03 copy Improvement

--- 02_programming_fundamentals/07_file_management/01_basic_fs/03_copy_file.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/03_copy_file.js
@@ -6,41 +6,38 @@
 
 const path = require("path");
 const fs = require("fs");
-const { COPYFILE_EXCL } = fs.constants;
 
 function copyPaste(sourceFilename, targetFilename){
 
+  if(!fs.existsSync(sourceFilename)) {
+    console.warn("File does not exist");
+    return;
+  } else if (fs.existsSync(targetFilename)) {
+    console.warn (`File ${targetFilename} already exists. We don't want to override it`);
+    return targetFilename;
+  }
+  console.log("ça passe");
   fs.stat(sourceFilename, (err,stats)=>{
     if(err){
-      console.log("error");
-    }else{
-      console.log(stats.isDirectory(sourceFilename));
-      if(stats.isDirectory(sourceFilename)){
-        console.log("We don't want to copy a repertory");
-        return;
-      }
-    }
-  });
-
-
-  fs.copyFile(sourceFilename, targetFilename, COPYFILE_EXCL,(err) => {
-    if (err){
-      if(err.code==="EEXIST"){
-        console.log (`File ${targetFilename} already exists. We don't want to override it`);
-        return;
-      }else{
-        console.log (err);
-        return;
-      }
-    }
-
-    else{
-      console.log(`${sourceFilename} was copied to ${targetFilename}`);
+      console.warn(err);
+      return err;
+    } else if (stats.isDirectory(sourceFilename)){
+      console.warn("We don't want to copy a folder");
       return;
     }
+
+    fs.copyFile(sourceFilename, targetFilename,(err) => {
+      if(err){
+        console.warn(err);
+        return;
+      }
+      console.log(`${sourceFilename} was copied to ${targetFilename}`);
+      return;
+    });
   });
 
-} // END OF copyPaste function
+}
 
-copyPaste ("testRepertory", "testText2.txt");
+
+copyPaste ("youpi23.txt", "test2.txt");
 module.exports = copyPaste;
