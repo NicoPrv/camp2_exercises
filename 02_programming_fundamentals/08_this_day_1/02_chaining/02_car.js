f0414ecfab1d46ab66c66bcf1f07c2f183676d23
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-12 11:50:33 +0200

    02_car OK

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,43 @@
 // See Sparta courses for the exercise summary
 
+
+const car={
+
+
+
+  start:function(){
+    this.distance = 0;
+    this.speed = 0 ;
+    this.drivingTime = 0 ;
+    this.distance = 0 ;
+    return this ;
+  },
+
+  changeSpeed:function(speed){
+    this.speed = speed ;
+    return this ;
+  },
+
+  drive:function(drivingTime){
+    this.drivingTime = drivingTime ;
+    this.distance  += this.speed * this.drivingTime / 60 ;
+    return this ;
+  },
+
+  showDistance:function(){
+    //this.distance += this.speed * this.drivingTime / 60 ;
+    console.log(this.distance + " Km");
+    //return this.distance + " Km";
+    return this ;
+  }
+
+
+
+};
+
+
+
+
+console.log (car.start().changeSpeed(60).drive(60).changeSpeed(30).drive(60).showDistance());
+
 module.exports = car;
