73dfb77cdfd9bc617c7512141ace95f2c2643e27
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-04-12 14:59:00 +0200

    04 Orange OK

--- 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
+++ 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
@@ -1,3 +1,65 @@
 // See Sparta courses for the exercise summary
+const orangeTree = {
+//ATTRIBUTES
+  seed : function () {
+    this.height = 0 ;
+    this.age = 0 ;
+    this.oranges = 0 ;
+    this.alive = true ;
+  },
+
+  pickAnOrange : function () {
+    if (this.oranges > 0) {
+      this.oranges-- ;
+      return true;
+    }
+    return false ;
+  },
+
+  ageOneYear : function () {
+    this.oranges = 0;
+    this.age ++ ;
+
+    if(this.age < 10){
+      this.height += 25 ;
+    } else if (this.age < 20){
+      this.height += 10 ;
+    } else {
+      this.height += 0 ;
+    }
+
+    if (this.age > 4 && this.age < 10){
+      this.oranges += 10 ;
+    } else if (this.age > 4 && this.age < 20){
+      this.oranges += 20 ;
+    } else if (this.age > 19 && this.age < 40) {
+      this.oranges += 5 ;
+    } else {
+      this.oranges += 0 ;
+    }
+
+    if (this.age > 50){
+      if(Math.random() > 0.5){
+        this.alive = true ;
+      }
+      else {
+        this.alive = false ;
+      }
+    }
+
+
+  }
+
+
+
+
+};
+
+
+
+
+
+
+
 
 module.exports = orangeTree;
